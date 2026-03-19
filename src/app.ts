b986778c05c83070c1d54b06054fd8e74937ea01
swapno963 <swapno963@gmail.com>
2026-03-19 22:13:32 +0600

    Cors origin related bug fixed

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import pool from "./db.js";
 import routes from "./routes.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use("/api", routes);
 
